bb8ec15e470d261c55f3c6769ae69dfff18d99e1
nostop10 <angeberresafou@gmail.com>
2024-02-19 14:12:42 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-*main - main block 
-*retourn: 0
-*/
-int main (void)
+ *main - main block 
+ * return: 0
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
