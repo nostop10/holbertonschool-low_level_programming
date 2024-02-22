533eee80ca1ac7e89cdd269c1c08b4eceb358590
nostop10 <angeberresafou@gmail.com>
2024-02-22 14:14:58 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  * return: 0
  */
 int main(void)
-{	
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -13,7 +13,7 @@ int main(void)
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_putchar('r'); 
+	_putchar('r');
 	_putchar('\n');
-	return (0); 
+	return (0);
 }
