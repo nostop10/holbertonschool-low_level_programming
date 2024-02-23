26f4681e9e0474d84b83cf666fa0e27bd6ce53df
nostop10 <angeberresafou@gmail.com>
2024-02-23 14:08:33 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
-#include "main.h" 
-/** 
- * print_alphabet_x10 - faire lalphabet 
- * 
- * Return: void 
- */ 
-void print_alphabet_x10(void) 
-{ 
-char c; 
-int i = 0; 
-while (i <= 9) 
-{ 
-for (c = 'a'; c <= 'z'; c++) 
-{  
-_putchar(c); 
-}			
+#include "main.h"
+/**
+ * print_alphabet_x10 - faire lalphabet
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+char c;
+int i = 0;
+while (i <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
 _putchar('\n');
 i++;
 }
