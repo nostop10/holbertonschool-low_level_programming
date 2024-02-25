feb74201d3dc44295fcc3e7370f76fe987aead14
nostop10 <angeberresafou@gmail.com>
2024-02-25 17:03:08 +0000

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * jack_bauer - Prints every minute of the day of
+ * Jack Bauer, starting from 00:00 to 23:59.
  * Return: void
  */
 void jack_bauer(void)
@@ -16,15 +16,13 @@ if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 for (c = 0; c <= 5; c++)
 {
 for (d = 0; d <= 9; d++)
-}
+{
 _putchar(a + '0');
 _putchar(b + '0');
 _putchar(58);
-_putchar(c + '0');							
+_putchar(c + '0');
 _putchar(d + '0');
 _putchar('\n');
-}
-}
 }
 }
 }
