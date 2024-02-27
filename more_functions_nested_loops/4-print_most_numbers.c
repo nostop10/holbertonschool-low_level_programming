3a7d738c77e29bcd540d1e1ffe3d67584924b6ed
nostop10 <angeberresafou@gmail.com>
2024-02-27 13:41:52 +0000

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_most_numbers(void) {
+    char chiffres[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    int i;
+
+    for (i = 0; i < 10; i++) {
+        if (chiffres[i] != '2' && chiffres[i] != '4') {
+            _putchar(chiffres[i]);
+        }
+    }
+
+    _putchar('\n');
+}
+
+int main() {
+    print_most_numbers();
+    return 0;
+}
