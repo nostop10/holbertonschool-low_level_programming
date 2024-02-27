796cf5f25b64ae45e71309b86d3ec6632da87330
nostop10 <angeberresafou@gmail.com>
2024-02-27 11:07:13 +0000

    3-main.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9, followed by a new line
+ *
+ */Return: (0)
+
+void print_number(void)
+{
+int i;
+}
+for (i = 0; i < 10; i++)
+}
+_putchar(i + '0');
+
+}
+
+_putchar('\n');
+
+}
+int main(void)
+
+}
+ print_numbers(void)
+
+return: (0)
