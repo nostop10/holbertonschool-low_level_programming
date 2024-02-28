84c6863280b0b301c5cb49c9805729dc682f1c6d
nostop10 <angeberresafou@gmail.com>
2024-02-28 11:39:42 +0000

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,16 +5,16 @@
  */
 void more_numbers(void)
 {
-	int c, d;
+int c, d;
 
-	for (c = 0; c < 10; c++)
-	{
-		for (d = 0; d < 15; d++)
-		{
-			if (d > 9)
-				_putchar(d / 10 + '0');
-			_putchar(d % 10 + '0');
-		}
-		_putchar('\n');
-	}
+for (c = 0; c < 10; c++)
+{
+for (d = 0; d < 15; d++)
+{
+if (d > 9)
+_putchar(d / 10 + '0');
+_putchar(d % 10 + '0');
+}
+_putchar('\n');
+}
 }
