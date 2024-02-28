a6b45c18edef828194e560c7dba43dcf75c4e3b1
nostop10 <angeberresafou@gmail.com>
2024-02-28 09:38:40 +0000

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,27 +1,15 @@
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: character to print
- * Return: 1 on success, -1 on error
+ *print_most_numbers - A function that prints the numbers, from 0 to 9
+ * Return: void
  */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
 void print_most_numbers(void)
 {
-    int num;
-    for (num = 0; num <= 9; num++)
-    {
-        if (num != 2 && num != 4)
-        {
-            _putchar(num + '0');
-        }
-    
- }
-    _putchar('\n');
-}
-int main(void)
+int num;
+for (num = 0; num <= 9; num++)
 {
-    print_most_numbers();
-    return (0);
+if (num != 2 && num != 4)
+_putchar(num + '0');
+}
+_putchar('\n');
 }
