b78a2add98d6021240345d44f7222521d724c896
nostop10 <angeberresafou@gmail.com>
2024-03-06 10:16:46 +0000

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+include "main.h"
+/**
+ * puts_half - prints half the string followed by a new line
+ * @str: the string being printed
+ */
+void puts_half(char *str)
+{
+	int n, ln = 0;
+	char *a;
+
+	while (*str != '\0')
+	{
+		str++;
+		ln++;
+	}
+	a = (str - ln);
+		n = ((ln + 1) / 2);
+	while (a[n] != '\0')
+	{
+		_putchar(a[n]);
+		n++;
+	}
+	_putchar('\n');
+}
