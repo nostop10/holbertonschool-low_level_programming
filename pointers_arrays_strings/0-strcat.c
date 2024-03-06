7fb71ff7e385a9657cebba2f0869ca8acf50e44c
nostop10 <angeberresafou@gmail.com>
2024-03-06 14:40:27 +0000

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: first input string
+ * @src: second input string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, len;
+
+	len = 0;
+	while (dest[len] != '\0')
+	{
+		++len;
+	}
+	for (i = 0; src[i] != '\0'; ++i, ++len)
+	{
+		dest[len] = src[i];
+	}
+	dest[len] = '\0';
+	return (dest);
+}
