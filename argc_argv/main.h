0bac222129f4871c6180e0c1218a08efeb702d3b
nostop10 <angeberresafou@gmail.com>
2024-03-13 11:57:50 +0000

    4-add.c

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -5,6 +5,10 @@ int _atoi(char *s);
 int i, d, n, len, f, digit;
 int main(int argc, char *argv[]);
 int result, num1, num2;
-
+int check_num(char *str);
+unsigned int count;
+int main(int argc, char *argv[]);
+int str_to_int;
+int sum = 0;
  
 
