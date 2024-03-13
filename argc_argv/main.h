09302497b51dc7df4d3dc19ce1598418344f0db9
nostop10 <angeberresafou@gmail.com>
2024-03-13 11:50:34 +0000

    3-mul.c

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,4 +1,10 @@
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
+int _atoi(char *s);
+int i, d, n, len, f, digit;
+int main(int argc, char *argv[]);
+int result, num1, num2;
+
+ 
 
