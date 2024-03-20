83cfbfb3d8ab4360879a8b884792c6c48f2cd01b
nostop10 <angeberresafou@gmail.com>
2024-03-20 11:04:15 +0000

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,23 +1,14 @@
+#include "function_pointers.h"
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-
-void printMessage(const char *name) {
-    printf("Hello, my name is %s\n", name);
-}
-
-void printUppercaseMessage(const char *name) {
-    char uppercaseName[strlen(name) + 1];
-    for (int i = 0; name[i] != '\0'; i++) {
-        uppercaseName[i] = toupper(name[i]);
-    }
-    uppercaseName[strlen(name)] = '\0';
-    printf("Hello, my uppercase name is %s\n", uppercaseName);
-}
-
-int main() {
-    char name[] = "Bob";
-    printMessage(name);
-    printUppercaseMessage(name);
-    return 0;
-}
+/**
+ * print_name - print name using pointer to function
+ * @name: string to add
+         * @f: pointer to function
+         * Return: nothing
+         **/
+        void print_name(char *name, void (*f)(char *))
+        {
+                if (name == NULL || f == NULL)
+                        return;
+                f(name);
+        }
