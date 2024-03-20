f670bd84ac38c29104b3958fb6f7c1b700ce732a
nostop10 <angeberresafou@gmail.com>
2024-03-20 11:15:41 +0000

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 /**
  * print_name - print name using pointer to function
- * @name: string to add
-         * @f: pointer to function
-         * Return: nothing
-         **/
-        void print_name(char *name, void (*f)(char *))
+ * @name: name of person  
+ * * @f:funtion to run
+ * Return: nothing
+ */
+     void print_name(char *name, void (*f)(char *))
         {
-                if (name == NULL || f == NULL)
-                        return;
+        
                 f(name);
         }
