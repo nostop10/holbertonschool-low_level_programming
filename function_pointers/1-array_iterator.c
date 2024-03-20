089d285ae8d0b0eba5e19d7eac52ea0720a94104
nostop10 <angeberresafou@gmail.com>
2024-03-20 14:43:34 +0000

    1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -8,12 +8,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-
 	unsigned int i;
-    
+
 	for (i = 0; i < size; i++)
 	{
-        
-    		action(array[i]);
+		action(array[i]);
 	}
 }
