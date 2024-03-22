d6c8483a4aae83c3879a058a05c4fda9adb793e6
nostop10 <angeberresafou@gmail.com>
2024-03-22 11:20:47 +0000

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,11 +3,10 @@
 /**
  * print_name - print name using pointer to function
  * @name: name of person  
- * * @f:funtion to run
+ * @f: funtion to run
  * Return: nothing
  */
      void print_name(char *name, void (*f)(char *))
-        {
-        
+        { 
                 f(name);
         }
